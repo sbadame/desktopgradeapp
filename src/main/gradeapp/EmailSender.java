bb4873e0ff4d8c01134cb3cddb87795986e3b777
Anthony DiFiore <anthonydifiore333@gmail.com>
2010-02-15 15:23:01 -0500

    Did Work

---
+++ src/main/gradeapp/EmailSender.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gradeapp;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author sandro
+ */
+// Ufa
+public class EmailSender {
+public static void sentEmail(){
+JOptionPane.showMessageDialog(null, "Inside Email Sender");
+}
+
+}
