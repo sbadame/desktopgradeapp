e3912a124d6ea58759a1fc10e20c4bfbae87af98
sbadame <s.badame@gmail.com>
2010-02-15 17:01:42 -0500

    Added example of how JGraph works, pretty friggen cool. target: example-graph to run

--- src/main/example/graph/JGraphTest.java
+++ src/main/example/graph/JGraphTest.java
@@ -1,20 +1,21 @@
 package example.graph;
 
+import com.jgraph.layout.JGraphFacade;
+import com.jgraph.layout.tree.JGraphTreeLayout;
 import java.awt.Color;
 import java.awt.geom.Rectangle2D;
+import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 import org.jgraph.JGraph;
-import org.jgraph.graph.DefaultCellViewFactory;
-import org.jgraph.graph.DefaultEdge;
-import org.jgraph.graph.DefaultGraphCell;
-import org.jgraph.graph.DefaultGraphModel;
-import org.jgraph.graph.DefaultPort;
-import org.jgraph.graph.GraphConstants;
-import org.jgraph.graph.GraphContext;
-import org.jgraph.graph.GraphLayoutCache;
-import org.jgraph.graph.GraphModel;
+import org.jgraph.graph.*;
 
+/**
+ * This code is based on the documentation found in jgraphmanual.pdf
+ * Specifically pages 106-113 for Tree Layout info and 17-22 for everything else
+ * @author Sandro Badame <a href="mailto:s.badame@gmail.com">s.badame&amp;gmail.com</a>
+ */
 public class JGraphTest extends JFrame {
 
     public JGraphTest(){
@@ -25,7 +26,6 @@ public class JGraphTest extends JFrame {
         GraphModel model  = new DefaultGraphModel();
         GraphLayoutCache view = new GraphLayoutCache(model, new DefaultCellViewFactory());
         JGraph graph = new JGraph(model, view);
-
 
         DefaultGraphCell[] cells = new DefaultGraphCell[3];
         //0
@@ -39,7 +39,7 @@ public class JGraphTest extends JFrame {
         //1
         cells[1] = new DefaultGraphCell("World");
         GraphConstants.setBounds(cells[1].getAttributes(), new Rectangle2D.Double(140,140,40,20));
-        GraphConstants.setGradientColor(cells[1].getAttributes(), Color.ORANGE);
+        GraphConstants.setGradientColor(cells[1].getAttributes(), Color.BLUE);
         GraphConstants.setOpaque(cells[1].getAttributes(), true);
         DefaultPort port1 = new DefaultPort();
         cells[1].add(port1);
@@ -54,6 +54,19 @@ public class JGraphTest extends JFrame {
         GraphConstants.setEndFill(edge.getAttributes(), true);
 
         graph.getGraphLayoutCache().insert(cells);
+
+        //Build our layout
+        JGraphTreeLayout layout = new JGraphTreeLayout();
+        layout.setAlignment(SwingConstants.TOP);
+        layout.setOrientation(SwingConstants.NORTH);
+        layout.setCombineLevelNodes(false);
+
+        //Do the tree layout here
+        JGraphFacade facade = new JGraphFacade(graph, new Object[]{});
+        layout.run(facade); //Run the layout on the facade
+        //Obtain a map of the resulting changes from the facade
+        Map nested = facade.createNestedMap(true, true);
+        graph.getGraphLayoutCache().edit(nested); //Apply changes
 
         JGraphTest jGraphTest = new JGraphTest();
         jGraphTest.getContentPane().add(new JScrollPane(graph));
