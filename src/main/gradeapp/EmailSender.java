983a31e80eb125b354d0f0b8b9c2476b4c3d6b8d
sbadame <s.badame@gmail.com>
2010-02-16 16:05:51 -0500

    I accidentaly commited the sin of all sins, I have reverted these changes and will repent for my misdeeds

--- src/main/gradeapp/EmailSender.java
+++ src/main/gradeapp/EmailSender.java
@@ -5,19 +5,12 @@
 
 package gradeapp;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
 import javax.swing.JOptionPane;
 import java.util.*;
+import java.io.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 
 /**
  *
@@ -26,85 +19,67 @@ import javax.swing.JProgressBar;
 // Most of Code Ripped From:
 // http://www.java-tips.org/other-api-tips/javamail/how-to-send-an-email-with-a-file-attachment.html
 public class EmailSender {
-    public static void sentEmail(){
-      // Change If Needed Here
-      String to = JOptionPane.showInputDialog("What email address should I send this to?");
-      String from = "bigantfbi333@optonline.net";
-      String host = "mail.optonline.net";
-      String filename = "gayjava.txt";
-      String msgText1 = "See Attachment For Grade Graph.\nDo Not Reply To This Email.\n";
-      String subject = "Grade Graph Results";
+public static void sentEmail(){
+JOptionPane.showMessageDialog(null, "Wait A Sec ... Hacking ... Sending Email ...");
 
-      //Show this progress bar
-      JProgressBar progress = new JProgressBar();
-      progress.setIndeterminate(true);
-      progress.setString("Sending...");
-      progress.setStringPainted(true);
+  // Change If Needed Here
+  String to = "desktopgradeapp@gmail.com";
+  String from = "bigantfbi333@optonline.net";
+  String host = "mail.optonline.net";
+  String filename = "gayjava.txt";
+  String msgText1 = "See Attachment For Grade Graph.\nDo Not Reply To This Email.\n";
+  String subject = "Grade Graph Results";
 
-      JPanel panel = new JPanel();
-      panel.add(progress);
+  // create some properties and get the default Session
+  Properties props = System.getProperties();
+  props.put("mail.smtp.host", host);
+  Session session = Session.getInstance(props, null);
 
-      
-      JDialog dialog = new JDialog((JFrame)null, "Sending Email");
-      dialog.setPreferredSize(new Dimension(175,100));
-      dialog.setLocationRelativeTo(null);
-      dialog.getContentPane().add(new JLabel("Working..."), BorderLayout.CENTER);
-      dialog.setSize(175, 100);
-      dialog.pack();
-      dialog.setVisible(true);
-      dialog.toFront();
+  try
+  {
+      // create a message
+      MimeMessage msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress(from));
+      InternetAddress[] address = {new InternetAddress(to)};
+      msg.setRecipients(Message.RecipientType.TO, address);
+      msg.setSubject(subject);
 
-      // create some properties and get the default Session
-      Properties props = System.getProperties();
-      props.put("mail.smtp.host", host);
-      Session session = Session.getInstance(props, null);
+      // create and fill the first message part
+      MimeBodyPart mbp1 = new MimeBodyPart();
+      mbp1.setText(msgText1);
 
-      try
-      {
-          // create a message
-          MimeMessage msg = new MimeMessage(session);
-          msg.setFrom(new InternetAddress(from));
-          InternetAddress[] address = {new InternetAddress(to)};
-          msg.setRecipients(Message.RecipientType.TO, address);
-          msg.setSubject(subject);
+      // create the second message part
+      MimeBodyPart mbp2 = new MimeBodyPart();
 
-          // create and fill the first message part
-          MimeBodyPart mbp1 = new MimeBodyPart();
-          mbp1.setText(msgText1);
+            // attach the file to the message
+         FileDataSource fds = new FileDataSource(filename);
+      mbp2.setDataHandler(new DataHandler(fds));
+      mbp2.setFileName(fds.getName());
 
-          // create the second message part
-          MimeBodyPart mbp2 = new MimeBodyPart();
+      // create the Multipart and add its parts to it
+      Multipart mp = new MimeMultipart();
+      mp.addBodyPart(mbp1);
+      mp.addBodyPart(mbp2);
 
-                // attach the file to the message
-             FileDataSource fds = new FileDataSource(filename);
-          mbp2.setDataHandler(new DataHandler(fds));
-          mbp2.setFileName(fds.getName());
+      // add the Multipart to the message
+      msg.setContent(mp);
 
-          // create the Multipart and add its parts to it
-          Multipart mp = new MimeMultipart();
-          mp.addBodyPart(mbp1);
-          mp.addBodyPart(mbp2);
+      // set the Date: header
+      msg.setSentDate(new Date());
 
-          // add the Multipart to the message
-          msg.setContent(mp);
+      // send the message
+      Transport.send(msg);
 
-          // set the Date: header
-          msg.setSentDate(new Date());
-
-          // send the message
-          Transport.send(msg);
-
+  }
+  catch (MessagingException mex)
+  {
+      mex.printStackTrace();
+      Exception ex = null;
+      if ((ex = mex.getNextException()) != null) {
+    ex.printStackTrace();
       }
-      catch (MessagingException mex)
-      {
-          mex.printStackTrace();
-          Exception ex = null;
-          if ((ex = mex.getNextException()) != null) {
-            ex.printStackTrace();
-          }
-      }
-      dialog.setVisible(false);
-      dialog.dispose();
-    }
+  }
+  JOptionPane.showMessageDialog(null, "Done Hacking ... Sent Email.");
+}
 
 }
