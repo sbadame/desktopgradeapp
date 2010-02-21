7fa30128790c545bb37b145f8f02127051b51eeb
sbadame <s.badame@gmail.com>
2010-02-21 16:20:02 -0500

    Forgot X2

---
+++ src/examples/boothe/BootheAlgo.java
@@ -0,0 +1,15 @@
+package boothe;
+
+import gradeapp.MinedTree;
+import java.io.IOException;
+
+public class BootheAlgo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        MinedTree.main(args);
+    }
+
+}
